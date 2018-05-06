d9e8ea7335f8d5bf2d5c4af1cf7d86d1d2bd41b3
Cyril <born2445@gmail.com>
2018-05-06 17:19:38 +0300

    Start

--- backpropagation/Form1.cs
+++ backpropagation/Form1.cs
@@ -16,5 +16,34 @@ namespace backpropagation
         {
             InitializeComponent();
         }
+        Bitmap bmp;
+        Brush brush;
+        Pen pen;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Brush brush = new SolidBrush(Color.Black);
+            pen = new Pen(brush, 11);
+            bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            for (int i = 0; i < pictureBox1.Width; i++)
+            {
+                for (int j = 0; j < pictureBox1.Height; j++)
+                {
+                    bmp.SetPixel(i, j, Color.White);
+                }
+            }
+            pictureBox1.Image = bmp;
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                using (Graphics gr = Graphics.FromImage(bmp))
+                {
+                    gr.DrawEllipse(pen, e.X, e.Y, 11, 11);
+                }
+                pictureBox1.Image = bmp;
+            }
+        }
     }
 }
